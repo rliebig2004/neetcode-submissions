a37b1e278c2f97ebc96d15711826d8142a3b69b5
Reynella Liebig <reynella.liebig@gmail.com>
2026-07-25 20:55:13 -0700

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        n = len(prices)
+        l = 0
+        r = 1
+        max_diff = 0
+
+        while r < n:
+            curr = prices[r] - prices[l]
+            if curr <= 0:
+                l = r
+                r += 1
+                continue
+            
+            elif curr > max_diff:
+                max_diff = curr
+                r += 1
+                continue
+
+            else:
+                r += 1
+
+        return max_diff
+
+
+            
+
+        
